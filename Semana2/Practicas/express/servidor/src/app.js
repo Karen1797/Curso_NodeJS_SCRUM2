fe09edbadfae60a0b0f8f46e9812f8d2b98a7006
Karen1797 <karen1797@gmail.com>
2019-03-24 14:51:35 -0500

    request

--- Semana2/Practicas/express/servidor/src/app.js
+++ Semana2/Practicas/express/servidor/src/app.js
@@ -21,14 +21,18 @@ app.get('/', (req, res) => {
 });
 
 app.get('/calculos',(req, res) => {
+    console.log(req.query);
     res.render('calculos', {
-        estudiante: 'Jaime',
+       /* estudiante: 'Jaime',
         nota1: 4,
         nota2: 5,
-        nota3: 3
+        nota3: 3 */
+        estudiante: req.query.nombre,
+        nota1: parseInt(req.query.nota1),
+        nota2: parseInt(req.query.nota2),
+        nota3: parseInt(req.query.nota3)
     });
 });
-
 
 app.listen(3000, () => {
     console.log('Escuchando por el puerto 3000');
