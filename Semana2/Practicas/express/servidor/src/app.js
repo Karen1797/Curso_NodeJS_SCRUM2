0697fdb490fe2fe612ece39f1769861cb110652a
Karen1797 <karen1797@gmail.com>
2019-03-23 19:08:32 -0500

    hbs

--- Semana2/Practicas/express/servidor/src/app.js
+++ Semana2/Practicas/express/servidor/src/app.js
@@ -1,11 +1,20 @@
 const express = require ('express');
 const app = express();
 const path = require ('path');
+const hbs = require('hbs');
 
 const directorioPublico = path.join(__dirname,'../public')
 app.use(express.static(directorioPublico));
 
-console.log(__dirname)
+//console.log(__dirname)
+
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        estudiante: 'Karen'
+    });
+});
 
 app.listen(3000, () => {
     console.log('Escuchando por el puerto 3000');
