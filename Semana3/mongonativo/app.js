f56f642e6c0311363056a5870d3e1b4cbc84af10
Karen1797 <karen1797@gmail.com>
2019-04-05 17:00:42 -0500

    CRUDmongo

--- Semana3/mongonativo/app.js
+++ Semana3/mongonativo/app.js
@@ -23,5 +23,96 @@ client.connect(function(err) {
 
   const collection = db.collection('estudiantes');
 
+  /*//Crear
+  collection.insertOne({
+      nombre:'Jaime',
+      matematicas: 3,
+      ingles: 4,
+      prrogramacion: 5,
+      lengua: 3
+  }, (err, result) => {
+      if(err){
+          return console.log('error ingresando usuario')
+      }
+      return console.log(result.ops)
+  })
+*/
+/*
+//Leer
+  collection.findOne({nombre: 'Jaime'},(err, resultado)=>{
+    if(err){
+        return console.log('error')
+    }if(resultado){
+        return console.log('no se encontrò el nombre')
+    }
+    console.log(resultado)
+  })
+
+  collection.find({matematicas:5}).toArray((err,resultados) => {
+    if(err){
+        return console.log('error')
+    }
+    console.log(resultados)
+  })
+*/
+
+//Actualizar
+/*
+  collection.updateOne({nombre: 'Jaime'},
+    {$set: {
+        ingles: 5,
+        programacion:3}}, 
+    (err, resultado)=>{
+        if(err){
+            return console.log('error en actualizar')
+        }
+        console.log('Se actualizo correctamente' + resultado) 
+    })
+
+    collection.updateMany({matematicas: 3},
+        {$set: {
+            matematicas:4
+        }},
+        (err, resultado)=>{
+            if(err){
+                return console.log('error en actualizar')
+            }
+            console.log('Se actualizo correctamente' + resultado) 
+        }
+    )
+*/
+    collection.find({}).toArray((err,resultados) => {
+        if(err){
+            return console.log('error en buscar')
+        }
+        console.log(resultados)
+    })
+    collection.deleteOne({nombre: 'Jaime'}, (err, resultado) =>{
+        if(err){
+            return console.log('error en elminar')
+        }
+        console.log('Se ha eliminado el elemento' + resultado) 
+    })
+    collection.find({}).toArray((err,resultados) => {
+        if(err){
+            return console.log('error')
+        }
+        console.log(resultados)
+        })
+
+        collection.deleteMany({matematicas: {$lt :4}}, (err, resultado) =>{
+            if(err){
+                return console.log('error en elminar')
+            }
+            console.log('Se ha eliminado el elemento' + resultado) 
+        })
+
+        collection.find({}).toArray((err,resultados) => {
+            if(err){
+                return console.log('error')
+            }
+            console.log(resultados)
+            })
   client.close();
 });
+
